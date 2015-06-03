27329ad3ef52a6f6bfd25e8a242f68c37f59ef88
Henner RS <henner.renardi@gmail.com>
2015-06-03 20:57:52 +0700

    1.0 - Test version

---
+++ README.md
@@ -0,0 +1,43 @@
+Blackboard - Sublime 3 Theme
+=============================================
+
+Inspired by Codecademy.com Editor.
+
+**Note**: This theme is still a work in progress and not in Package Control yet.
+
+![Eightlime Dark - HTML](http://cdn.setyono.net/blackboard/dark-html.jpg)
+
+Installation
+-------------------
+
+1. Download the 
+
+**Setting up the Theme**:
+
+1. Change the color scheme by going to: `Preferences > Color Scheme > Theme - Blackboard > Blackboard Dark`
+
+2. Change the interface by going to `Preferences > Setting - User`
+
+It will open a text file. Add the following line to the comma-separated list:
+
+    "theme": "Blackboard Dark.sublime-theme"
+
+Overall, mine looks like this:
+    
+    {
+      "color_scheme": "Packages/Theme - Blackboard/Blackboard Dark.tmTheme",
+      "ignored_packages":
+      [
+        "Vintage"
+      ],
+      "theme": "Blackboard Dark.sublime-theme",
+    }
+
+License and Credit
+-------------------
+
+Blackboard is MIT Licensed and based on:
+
+- [Dark Eight](https://github.com/ShawnMcCool/theme-dark-eight)
+
+- [Codecademy](http://codecademy.com/)
---
+++ sample/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Untitled</title>
+</head>
+<body>
+  <!-- Main Content -->
+  <article class="hello">
+    <h1>Hello World</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, fugiat, reprehenderit eligendi ea amet eaque quasi iure numquam accusantium nulla itaque dolore tenetur nisi nihil at.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, illo veniam non amet dolores maiores.
+    </p>
+  </article>
+  
+  <!-- Script -->
+  <script>
+    function foo() {
+      var myText = "Lorem ipsum dolor sit amet.";
+      console.log(myText);
+    }
+  </script>
+
+  <?php
+    class Spinach extends Vegetable {
+      var $cooked = false;
+
+      function Spinach(){
+        $this->Vegetable(true, "green");
+      }
+
+      function cook_it() {
+        $this->cooked = true;
+      }
+
+      function is_cooked() {
+        return $this->cooked;
+      }
+    } // end of class Spinach
+  ?>
+</body>
+</html>
